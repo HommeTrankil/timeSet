c94ac1f18f9556447b3499c4d1f7f617f27376dd
HommeTrankil <dj.danjosse@gmail.com>
2018-12-16 09:46:17 -0500

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import timePick , timeTest , sugarTest
+
+# Register your models here.
+admin.site.register(timePick)
+admin.site.register(timeTest)
+admin.site.register(sugarTest)
